e5d469c052261f3851a20f9778cf684ed5fc6a7a
joaovbds4 <105249003+joaovbdss69@users.noreply.github.com>
2023-09-23 14:10:37 -0300

    Setup Projeto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto para realizar previsões sobre rounds no CS:GO',
+    author='Joao Vitor',
+    license='MIT',
+)
